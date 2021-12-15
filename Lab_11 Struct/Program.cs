85aa6110e633d073e3169a6fdc7e33ef4a02596d
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-15 18:35:47 +0300

    Добавьте файлы проекта.

---
+++ Lab_11 Struct/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Разработать структуру для решения линейного уравнения 0=kx+b.
+ * Коэффициенты уравнения k, b реализовать с помощью полей вещественного типа. 
+ * Для решения уравнения предусмотреть метод Root.
+ * Создать экземпляр разработанной структуры. Осуществить использование экземпляра в программе.*/
+namespace Lab_11_Struct
+{
+    struct Equation
+    {
+        public double k;
+        public double b;
+        
+        
+        public double Root()
+        {
+            double x=-b/k;
+            return x;
+        }
+    }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите переменные k и b");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double aa = Convert.ToDouble(Console.ReadLine());
+            Equation eq = new Equation { k=a, b=aa };
+            double x=eq.Root();
+            Console.WriteLine("Корень уравнения\n"+x);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab_11 Struct/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab_11 Struct")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab_11 Struct")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c584609e-5500-4262-b19d-fef74346d180")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
